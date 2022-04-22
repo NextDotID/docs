ca545fd22b6d331ae3ba0bc63498ef2d6dcd0063
Nyk Ma <nykma@mask.io>
2022-04-22 14:54:39 +0800

    [!] config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'nextdotid', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
+  trailingSlash: false,
 
   presets: [
     [
