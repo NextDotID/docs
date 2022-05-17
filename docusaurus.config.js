f13cae308cfd982215f215f7104bbcd25704e914
Yan <picturepan2@hotmail.com>
2022-05-17 16:10:23 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
